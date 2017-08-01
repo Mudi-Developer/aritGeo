0cd30035d2470e9253fded4bcf72cc07d95a7c67
my name <myemailid@domain.com>
2017-08-01 17:29:33 +0100

    modified code and tests

--- src/aritGeo.js
+++ src/aritGeo.js
@@ -1,32 +1,40 @@
-'use strict';
+/**
+     * The function checks the difference and ratio of each element
+     * in the provided array. If at the end of the check the 'val' is
+     * 'Arithmetic', it prints that. Same for the others. If the length of the
+     * array is 0 it prints 0
+     */
+    const aritGeo = (arr) => 
+    {
+      if (arr%2 == 0) return 'even integer';
+      if (arr%2 == 1) return 'odd integer';
+      if (arr && typeof arr === 'object' && arr.constructor === object) return 'object input is invalid';
+      if (arr && typeof arr === 'object' && arr.constructor === RegExp) 
+                    return 'RegExp input is invalid';
+      if (typeof arr === 'boolean') return 'boolean input is invalid';
+      if (typeof arr === 'undefined') return 'undefined';
+      if (typeof arr === 'null') return 'empty input';
+      if (typeof arr === 'function') return 'function input is invalid';
+      if (typeof arr === 'number') return 'number input is invalid';
+      if (typeof arr === 'string') return 'string input is invalid';
 
- /**
- * The function checks the difference and ratio of each element
- * in the provided array. If at the end of the check the 'val' is
- * 'Arithmetic', it prints that. Same for the others. If the length of the
- * array is 0 it prints 0
- */
-const aritGeo = (arr) => {
-  
-   if (!Array.isArray(arr)) return 'Input must not be empty';
-   if (arr.length === 0) return 0;
+      if (arr.length == 0) return 0; // case of array is empty
+      
 
+      let val;
 
-   let value; // value for checks
-   let commonDifference = arr[1] - arr[0]; // arithmetic ratio
-   let commonRatio = arr[1] / arr[0]; // geometric ratio
+      // For loop that loops through the array and performs the mathematical 
+      // calculations on the values of the array
+      for (let i = 0; i < arr.length - 2; i++) 
+      {
+        if (arr[i] - arr[i + 1] == arr[i + 1] - arr[i + 2]) 
+          val = 'Arithmetic';
+        else if (arr[i] / arr[i + 1] == arr[i + 1] / arr[i + 2]) 
+          val = 'Geometric';
+        else val = -1;
+      }
 
-   // For loop that loops through the array and performs the mathematical 
-            // calculations on the values of the array
-   for (let i = 0; i < arr.length - 1; i++) {
-       if (arr[i] + commonDifference === arr[i + 1]) { // check for arithmetic
-           value = 'Arithmetic';
-       } else if (arr[i] * commonRatio === arr[i + 1]) { // check for geometric
-           value = 'Geometric';
-       } else value = -1; // check for neither arithmetic/geometric
-   }
-   // return value
-   return value;
-};
+      return val;
+    }
 
-module.exports = aritGeo;
+    module.exports = aritGeo;
--- test/test.js
+++ test/test.js
@@ -6,16 +6,43 @@ describe('aritGeo([])', () =>{
 		assert.equal(aritGeo([]), 0)
 	});
 
-	it('Geometric', function(){		
+	it('It should return 0', () =>{		
+		assert.equal(aritGeo('stringValue'), 0)
+	});
+
+	it('It should return 0', () =>{		
+		assert.equal(aritGeo(true), 'boolean')
+	});
+
+	it('It should return 0', () =>{		
+		assert.equal(aritGeo(5), 'number')
+	});
+
+	it('It should return 0', () =>{		
+		assert.equal(aritGeo(5 / 0), 'undefined array')
+	});
+
+	it('It should return 0', () =>{		
+		assert.equal(aritGeo(), 'null array')
+	});
+
+	it('It should return 0', () =>{		
+		assert.equal(aritGeo(3), 'odd integer')
+	});
+
+	it('It should return 0', () =>{		
+		assert.equal(aritGeo(4), 'even integer')
+	});
+
+	it('Geometric', () =>{		
 		assert.equal(aritGeo([1,2,4,8,16]), 'Geometric')
 	});
 
-	it('Arithmetic', function(){		
+	it('Arithmetic', () =>{		
 		assert.equal(aritGeo([1,2,3,4,5]), 'Arithmetic')
 	});
 
-	it('neither', function(){		
+	it('neither', () =>{		
 		assert.equal(aritGeo([1,2,3,4,7]), -1)
 	});	
-
 });
